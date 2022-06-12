3c2057b3142029e488cc437664f576bea2ba16be
Lisha Liang <lishaliang1999@gmail.com>
2022-06-12 20:40:59 +0000

    breed

--- api/breedFetcher.js
+++ api/breedFetcher.js
@@ -1,15 +1,36 @@
 const request = require('request');
-const args = process.argv[2];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage
-  
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-});
+const fetchBreedDescription = function (breedName, callback) {
 
-console.log(args)
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body.length); // Print the HTML for the Google homepage
+
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, undefined)
+      return
+    }
+    else if (data.length === 0) {
+      callback(true, undefined)
+      return
+    }
+    else {
+      const desc = data[0].description;
+      callback(error, desc)
+    }
+    
+
+    // console.log(data);
+    // console.log(typeof data);
+  });
+
+};
+
+
+
+
+// console.log(args)
+module.exports = { fetchBreedDescription };
