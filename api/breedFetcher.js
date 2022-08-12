2b3d77f6a2bb642b1665e303f62b22945370c39f
Lisha Liang <lishaliang1999@gmail.com>
2022-08-12 17:14:54 +0000

    fetch function

--- api/breedFetcher.js
+++ api/breedFetcher.js
@@ -1,36 +1,27 @@
-const request = require('request');
-
+const request = require("request");
 
 const fetchBreedDescription = function (breedName, callback) {
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body.length); // Print the HTML for the Google homepage
-
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error, response, body) => {
     const data = JSON.parse(body);
+
+    // if (data[0]) {
+    //   console.log(data[0].description);
+    // } else {
+    //   console.log("breed not found");
+    // }
+
     if (error) {
+      console.log("error:", error); // Print the error if one occurred
       callback(error, undefined)
-      return
-    }
-    else if (data.length === 0) {
+    } else if (data.length === 0) {
       callback(true, undefined)
-      return
-    }
-    else {
+    } else {
       const desc = data[0].description;
       callback(error, desc)
     }
-    
 
-    // console.log(data);
-    // console.log(typeof data);
+
   });
-
 };
 
-
-
-
-// console.log(args)
 module.exports = { fetchBreedDescription };
